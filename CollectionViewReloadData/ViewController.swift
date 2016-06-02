68a6e9df29baaef8423c3da9ffe5fbc66598cc2b
iamsea <sea@iamsea.local>
2016-06-03 01:18:02 +0800

    CollectionView刷新

--- CollectionViewReloadData/ViewController.swift
+++ CollectionViewReloadData/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images = Array<UIImage>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.alwaysBounceVertical = true
+        let refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "下拉以刷新")
+        refreshControl.tintColor = UIColor.orangeColor()
+        refreshControl.addTarget(self, action: #selector(refresh), forControlEvents: UIControlEvents.ValueChanged)
+        collectionView.addSubview(refreshControl)
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func refresh(sender: UIRefreshControl) {
+        sender.attributedTitle = NSAttributedString(string: "正在更新...")
+        dispatch_async(dispatch_get_main_queue()) { () ->Void in
+            sleep(1)
+            sender.endRefreshing()
+            sender.attributedTitle = NSAttributedString(string: "下拉以刷新")
+        }
+    }
+    
+    func loadData() {
+        
+        for index in 0...4 {
+            images.append(UIImage(named: String(index))!)
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PCell", forIndexPath: indexPath)
+        let imageView = cell.viewWithTag(100) as! UIImageView
+        imageView.image = images[indexPath.item]
+        return cell
+    }
 }
 
